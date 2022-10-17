7b7753d53a968a0969caf7eb034ca5052a61da0d
irina <irina.kiselevitch@yandex.ru>
2022-10-17 15:00:33 +0300

    Вторая задача

---
+++ dz4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while (n>0)
+{
+    summa = summa + n % 10;
+    n=n/10;
+}
+
+Console.WriteLine(summa);
