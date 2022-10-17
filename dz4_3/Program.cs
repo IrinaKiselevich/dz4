6b92e18298f6c17c75d4ce6d68989b203895666a
irina <irina.kiselevitch@yandex.ru>
2022-10-17 15:08:34 +0300

    Третья задача

---
+++ dz4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите размер массива : ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] FillArray()
+{
+    int[] array = new int[size];
+    for (int i=0;i<size;i++)
+   {
+    array[i]=new Random().Next(0,9);
+   }
+   return array;
+}
+ void PrintArray(int[] array)
+    {
+        for(int i =0;i<size;i++)
+        if(i<size-1)
+        Console.Write($"{array[i]}, ");
+        else
+        Console.Write($"{array[i]}");
+    }
+int[] massiv = FillArray();
+PrintArray(massiv);
